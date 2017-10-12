b8fa1c561968f2929d919172bb73d66a85eb052f
kevinpei <kevinsongpei@gmail.com>
2017-10-11 20:36:07 -0400

    Made MLP multiple layers

--- assignment_2/question_1/mlp.py
+++ assignment_2/question_1/mlp.py
@@ -13,11 +13,12 @@ class MLPClassifier:
     self.legalLabels = legalLabels
     self.type = "mlp"
     self.max_iterations = max_iterations
-	self.layer_one_weights = {}
-	self.layer_two_weights = {}
-    for label in legalLabels:
-      self.layer_one_weights[label] = util.Counter() # this is the data-structure you should use
-	  self.layer_two_weights[label] = util.Counter()
+	self.layer_number = 5
+	self.layer_weights = {}
+	for layer in range(self.layer_number):
+	  self.layer_weights[layer] = {}
+      for label in legalLabels:
+        self.layer_weights[layer][label] = util.Counter() # this is the data-structure you should use
       
   def train( self, trainingData, trainingLabels, validationData, validationLabels ):
   
@@ -25,10 +26,10 @@ class MLPClassifier:
     # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING
 	
 # Initialize the weights for both layers of the perceptrons to be either 0 or 1 randomly
-  for x in range(10):
-	for y in self.features:
-	  self.layer_one_weights[x][y] = r.randint(0,1)
-	  self.layer_two_weights[x][y] = r.randint(0,1)
+  for layer in range(self.layer_number):
+    for x in range(10):
+	  for y in self.features:
+	    self.layer_weights[layer][x][y] = r.randint(0,1)
   
     for iteration in range(self.max_iterations):
       print "Starting iteration ", iteration, "..."
@@ -38,20 +39,23 @@ class MLPClassifier:
         sums = util.Counter()
 #		Multiply for each label 0-9
 	    for j in range(10):
-          layer_one = trainingData[i] * self.layer_one_weights[j]
-		  sums[j] = layer_one * self.layer_two_weights[j]
+		  value = trainingData[i] * self.layer_weights[0][j]
+		  for x in range(1, self.layer_number):
+		    value *= self.layer_weights[x][j]
+		  sums[j] = value
 #		If the label was incorrect, then decrease the weights of the obtained label and increase the weights of the correct label
         if sums.argMax() != trainingLabels[i]:
-          self.layer_one_weights[sums.argMax()] -= trainingData[i]
-          self.layer_one_weights[trainingLabels[i]] += trainingData[i]
+		  self.layer_weights[0][sums.argMax()] -= trainingData[i]
+		  self.layer_weights[0][trainingLabels[i]] += trainingData[i]
 		  
 		  #EDIT THIS PART IT'S PROBABLY WRONG
-		  wrong_label_error = self.layer_two_weights[sums.argMax()] - self.layer_one_weights[sums.argMax()]
-		  right_label_error = self.layer_two_weights[trainingLabels[i]] - self.layer_one_weights[trainingLabels[i]]
+		  for x in range(1, self.layer_number):
+		    wrong_label_error = self.layer_weights[x][sums.argMax()] - self.layer_weights[x-1][sums.argMax()]
+			right_label_error = self.layer_weights[x][trainingLabels[i]] - self.layer_weights[x-1][trainingLabels[i]]
+			self.layer_weights[x][sums.argMax()] -= trainingData[i] + wrong_label_error * self.layer_weights[x][sums.argMax()]
+			self.layer_weights[x][trainingLabels[i]] += trainingData[i] + right_label_error * self.layer_weights[x][trainingLabels[i]]
 		  #END EDITING PART
-		  
-		  self.layer_two_weights[sums.argMax()] -= trainingData[i] + wrong_label_error * self.layer_two_weights[sums.argMax()]
-          self.layer_two_weights[trainingLabels[i]] += trainingData[i] + right_label_error * self.layer_two_weights[trainingLabels[i]]
+
     
   def classify(self, data ):
     guesses = []
