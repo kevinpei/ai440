687918ba3541f90e2aa1eb19b7c9a72c0188f9e4
kevinpei <kevinsongpei@gmail.com>
2017-10-15 10:46:27 -0400

    Modified mlp a little

--- assignment_2/question_1/mlp.py
+++ assignment_2/question_1/mlp.py
@@ -40,30 +40,46 @@ class MLPClassifier:
         sums = util.Counter()
 #		Multiply for each label 0-9
         for j in range(10):
-		      value = trainingData[i] * self.layer_weights[0][j]
-		      for x in range(1, self.layer_number):
-		        value *= self.layer_weights[x][j]
-		        sums[j] = value
+          intermediate_value = util.Counter();
+          for feature in self.features:
+            intermediate_value[feature] = trainingData[i][feature] * self.layer_weights[0][j][feature]
+          for x in range(1, self.layer_number - 1):
+            for feature in self.features:
+              intermediate_value[feature] = intermediate_value[feature] * self.layer_weights[x][j][feature]
+          value = intermediate_value * self.layer_weights[self.layer_number - 1][j]
+          sums[j] = value
 #		If the label was incorrect, then decrease the weights of the obtained label and increase the weights of the correct label
         if sums.argMax() != trainingLabels[i]:
-		      self.layer_weights[0][sums.argMax()] -= trainingData[i]
-		      self.layer_weights[0][trainingLabels[i]] += trainingData[i]
+          self.layer_weights[0][sums.argMax()] -= trainingData[i]
+          self.layer_weights[0][trainingLabels[i]] += trainingData[i]
 		  
 		  #EDIT THIS PART IT'S PROBABLY WRONG
-        for x in range(1, self.layer_number):
-          wrong_label_error = self.layer_weights[x][sums.argMax()] - self.layer_weights[x-1][sums.argMax()]
-          right_label_error = self.layer_weights[x][trainingLabels[i]] - self.layer_weights[x-1][trainingLabels[i]]
-          self.layer_weights[x][sums.argMax()] -= trainingData[i] + wrong_label_error * self.layer_weights[x][sums.argMax()]
-          self.layer_weights[x][trainingLabels[i]] += trainingData[i] + right_label_error * self.layer_weights[x][trainingLabels[i]]
+          for x in range(1, self.layer_number):
+            wrong_label_error = self.layer_weights[x][sums.argMax()] - self.layer_weights[x-1][sums.argMax()]
+            right_label_error = self.layer_weights[x][trainingLabels[i]] - self.layer_weights[x-1][trainingLabels[i]]
+            wrong_data = util.Counter()
+            right_data = util.Counter()
+            for feature in self.features:
+              wrong_data[feature] = wrong_label_error[feature] * self.layer_weights[x][sums.argMax()][feature]
+              right_data[feature] = right_label_error[feature] * self.layer_weights[x][trainingLabels[i]][feature]
+            self.layer_weights[x][sums.argMax()] -= wrong_data
+            self.layer_weights[x][trainingLabels[i]] += right_data
 		  #END EDITING PART
 
     
   def classify(self, data ):
+    self.features = data[0].keys()
+  
     guesses = []
     for datum in data:
       vectors = util.Counter()
+      intermediate_value = util.Counter()
       for l in self.legalLabels:
-        layer_one = datum * self.layer_one_weights[j]
-        vectors[l] = layer_one * self.layer_two_weights[j]
+        for feature in self.features:
+          intermediate_value[feature] = datum[feature] * self.layer_weights[0][l][feature]
+        for x in range(1, self.layer_number - 1):
+          for feature in self.features:
+            intermediate_value[feature] = intermediate_value[feature] * self.layer_weights[x][l][feature]
+        vectors[l] = intermediate_value * self.layer_weights[self.layer_number - 1][l]
       guesses.append(vectors.argMax())
     return guesses
