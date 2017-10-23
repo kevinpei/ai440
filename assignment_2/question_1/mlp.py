b3356e49b2a0efc44d3345999aa52a76cc905729
kevinpei <kevinsongpei@gmail.com>
2017-10-22 21:29:21 -0400

    Updated mlp to use sigmoid function for intermediate layer

--- assignment_2/question_1/mlp.py
+++ assignment_2/question_1/mlp.py
@@ -4,6 +4,7 @@
 # mlp implementation
 import util
 import random as r
+import math as m
 PRINT = True
 
 class MLPClassifier:
@@ -14,20 +15,22 @@ class MLPClassifier:
     self.legalLabels = legalLabels
     self.type = "mlp"
     self.max_iterations = max_iterations
-    self.layer_number = 5
     self.layer_weights = {}
-    for layer in range(self.layer_number):
+    for layer in range(2):
       self.layer_weights[layer] = {}
       for label in legalLabels:
         self.layer_weights[layer][label] = util.Counter() # this is the data-structure you should use
       
+  def sigmoid(value):
+      return 1/(1 + m.exp(value))
+	
   def train( self, trainingData, trainingLabels, validationData, validationLabels ):
   
     self.features = trainingData[0].keys() # could be useful later
     # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING
 	
   # Initialize the weights for both layers of the perceptrons to be either 0 or 1 randomly
-    for layer in range(self.layer_number):
+    for layer in range(2):
       for x in range(10):
         for y in self.features:
           self.layer_weights[layer][x][y] = r.randint(0,1)
@@ -42,28 +45,20 @@ class MLPClassifier:
         for j in range(10):
           intermediate_value = util.Counter();
           for feature in self.features:
-            intermediate_value[feature] = trainingData[i][feature] * self.layer_weights[0][j][feature]
-          for x in range(1, self.layer_number - 1):
-            for feature in self.features:
-              intermediate_value[feature] = intermediate_value[feature] * self.layer_weights[x][j][feature]
-          value = intermediate_value * self.layer_weights[self.layer_number - 1][j]
+            intermediate_value[feature] = self.sigmoid(trainingData[i][feature] * self.layer_weights[0][j][feature])
+          value = intermediate_value * self.layer_weights[1][j]
           sums[j] = value
 #		If the label was incorrect, then decrease the weights of the obtained label and increase the weights of the correct label
         if sums.argMax() != trainingLabels[i]:
-          self.layer_weights[0][sums.argMax()] -= trainingData[i]
-          self.layer_weights[0][trainingLabels[i]] += trainingData[i]
+          self.layer_weights[1][sums.argMax()] -= trainingData[i]
+          self.layer_weights[1][trainingLabels[i]] += trainingData[i]
 		  
 		  #EDIT THIS PART IT'S PROBABLY WRONG
-          for x in range(1, self.layer_number):
-            wrong_label_error = self.layer_weights[x][sums.argMax()] - self.layer_weights[x-1][sums.argMax()]
-            right_label_error = self.layer_weights[x][trainingLabels[i]] - self.layer_weights[x-1][trainingLabels[i]]
-            wrong_data = util.Counter()
-            right_data = util.Counter()
-            for feature in self.features:
-              wrong_data[feature] = wrong_label_error[feature] * self.layer_weights[x][sums.argMax()][feature]
-              right_data[feature] = right_label_error[feature] * self.layer_weights[x][trainingLabels[i]][feature]
-            self.layer_weights[x][sums.argMax()] -= wrong_data
-            self.layer_weights[x][trainingLabels[i]] += right_data
+		  for feature in self.features:
+		    wrong_output = self.sigmoid(self.layer_weights[0][sums.argMax()][feature] * trainingData[i][feature])
+            self.layer_weights[0][sums.argMax()][feature] -= trainingData[i][feature] * wrong_output * (1 - output)
+			correct_output = self.sigmoid(self.layer_weights[0][trainingLabels[i]][feature] * trainingData[i][feature])
+			self.layer_weights[0][trainingLabels[i]][feature] += trainingData[i][feature] * correct_output * (1 - output)
 		  #END EDITING PART
 
     
@@ -76,10 +71,7 @@ class MLPClassifier:
       intermediate_value = util.Counter()
       for l in self.legalLabels:
         for feature in self.features:
-          intermediate_value[feature] = datum[feature] * self.layer_weights[0][l][feature]
-        for x in range(1, self.layer_number - 1):
-          for feature in self.features:
-            intermediate_value[feature] = intermediate_value[feature] * self.layer_weights[x][l][feature]
-        vectors[l] = intermediate_value * self.layer_weights[self.layer_number - 1][l]
+          intermediate_value[feature] = self.sigmoid(datum[feature] * self.layer_weights[0][l][feature])
+        vectors[l] = intermediate_value * self.layer_weights[1][l]
       guesses.append(vectors.argMax())
     return guesses
